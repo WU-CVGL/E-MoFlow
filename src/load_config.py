efed092514a66107e0bc942c078833a9da5c698a
akawincent <liwenpu0520@gmail.com>
2024-09-10 22:43:49 +0800

    feat: add config loader

---
+++ src/load_config.py
@@ -0,0 +1,22 @@
+import yaml
+import argparse
+
+def load_yaml_config(file_path):
+    with open(file_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def parse_args(config):
+    parser = argparse.ArgumentParser(description="configs for NeuEF")
+
+    parser.add_argument('--data_path', type=str, default=config.get('data_path', None), 
+                        help='the path to event data')
+    parser.add_argument('--gpu', type=int, default=config.get('gpu', 0),
+                        help='index of gpu')
+    parser.add_argument('--pe', type=int, default=config.get('pe', 0),
+                        help='set 0 for default positional encoding, -1 for none')
+    parser.add_argument('--multires', type=int, default=config.get('multires', 10),
+                        help='log2 of max freq for positional encoding (3D location)')
+
+    args = parser.parse_args()
+    return args
