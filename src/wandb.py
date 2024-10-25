49e04a2d51ac004412161805a9ea59a2efa2e9f5
akawincent <liwenpu0520@gmail.com>
2024-10-25 20:09:38 +0800

    perf: view loss on wandb

---
+++ src/wandb.py
@@ -0,0 +1,29 @@
+import wandb
+
+class WandbLogger:
+    def __init__(self, config) -> None:
+        wandb_conifg = config["logger"]
+        self.run = wandb.init(project=wandb_conifg["project"], config=config, name=wandb_conifg["expname"])
+        self.buffer = dict()
+
+    def write(self, label: str, value) -> None:
+        self.buffer[label] = value
+
+    def write_img(self, label: str, img) -> None:
+        img = wandb.Image(img, caption="mid")
+        self.buffer[label] = img
+
+    def write_imgs(self, label: str, imgs) -> None:
+        wandbimgs = []
+        for i, img in enumerate(imgs):
+            wandbimgs.append(wandb.Image(img, caption=str(i)))
+        self.buffer[label] = wandbimgs
+
+    def update_buffer(self):
+        self.run.log(self.buffer)
+        self.buffer = dict()
+
+    def write_checkpoint(self, path, expname):
+        artifact = wandb.Artifact(name=f'checkpoint_{expname}', type="model")
+        artifact.add_file(path)
+        self.run.log_artifact(artifact)
