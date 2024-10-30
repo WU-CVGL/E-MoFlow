004f11cf131c56801ff1996a53d4accc5c4f0bda
akawincent <liwenpu0520@gmail.com>
2024-10-30 21:26:49 +0800

    perf: select time reference near to events data

--- src/model/warp.py
+++ src/model/warp.py
@@ -2,28 +2,31 @@ import torch
 
 from .inr import EventFlowINR
 
-EPS = 1e-5
+EPS = 1e-3
 
 class NeuralODEWarp:
     def __init__(self, flow_inr: EventFlowINR, device: torch.device) -> None:
         self.flow_calculator = flow_inr       # calculate dy/dt
         self.device = device
-        self.num_step = 50
+        self.num_step = 5
 
     def get_reference_time(self, events: torch.Tensor) -> dict:
-        batch_xy0, batch_t0 = events[:, 1:], events[:, 0]
-        t_min, t_max = torch.min(batch_t0) - EPS, torch.max(batch_t0) + EPS
-        t_ref_1 = t_min * torch.rand(1).to(self.device)
-        t_ref_2 = t_max + (1 - t_max) * torch.rand(1).to(self.device)
+        t_min, t_max = torch.min(events[:, 0]), torch.max(events[:, 0])
+        return torch.clamp(t_max + EPS, min=0.0, max=1.0)
+        # events_time = events[:, 0]
+        # batch_xy0, batch_t0 = events[:, 1:], events[:, 0]
+        # t_min, t_max = torch.min(batch_t0) - EPS, torch.max(batch_t0) + EPS
+        # t_ref_1 = t_min * torch.rand(1).to(self.device)
+        # t_ref_2 = t_max + (1 - t_max) * torch.rand(1).to(self.device)
         
-        choice = torch.randint(0, 2, (1,)).item()
-        if choice == 0:
-            output = {"t_ref": t_ref_1, "dir": "backward"}
-        else: 
-            output = {"t_ref": t_ref_2, "dir": "forward"}
-        return output
+        # choice = torch.randint(0, 2, (1,)).item()
+        # if choice == 0:
+        #     output = torch.clamp(t_min - EPS, min=0.0, max=1.0)
+        # else: 
+        #     output = torch.clamp(t_max + EPS, min=0.0, max=1.0)
+        # return output
 
-    def warp_events(self, batch_txy: torch.Tensor, t_ref, dir):
+    def warp_events(self, batch_txy: torch.Tensor, t_ref):
         batch_t0 = batch_txy[:, 0]  
         warped_batch_txy = batch_txy.clone()
         t_step: torch.Tensor = ((t_ref - batch_t0) / self.num_step).unsqueeze(1)
