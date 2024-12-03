923e138d5df26b13f69ddb588a8387472145a2ff
akawincent <3511606256@qq.com>
2024-12-03 21:52:02 +0800

    feat: add new dataset type

--- src/dataset_loader/dataset_manager.py
+++ src/dataset_loader/dataset_manager.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader
 from .base import DatasetBase
 from .simple_test.reader import SimpleTestDataset
 from .simple_test.provider import SimpleTestDataProvider
+from .synthetic_figure.reader import SyntheticFigureDataset
+from .synthetic_figure.provider import SyntheticFigureDataProvider
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +16,9 @@ def get_dataset(dataset_name, data_config):
     if dataset_name == "SimpleTest":
         logger.info(f"Use SimpleTest datset.")
         return SimpleTestDataset(**data_config)
+    elif dataset_name == "SyntheticFigure":
+        logger.info(f"Use SyntheticFigure datset.")
+        return SyntheticFigureDataset(**data_config)
     elif dataset_name == "DSEC":
         pass
     elif dataset_name == "MVSEC":
@@ -30,9 +35,17 @@ def create_loader(dataset: DatasetBase, config: dict):
     if dataset.name == "SimpleTest":
         dataprovider = SimpleTestDataProvider(dataset)
         logger.info(f"The loader for {dataset.name} is created.")
+    elif dataset.name == "SyntheticFigure":
+        dataprovider = SyntheticFigureDataProvider(dataset)
+        logger.info(f"The loader for {dataset.name} is created.")
     else:
         e = f"Can't creat loader for {dataset.name}."
         logger.error(e)
         raise ValueError(e)        
     
-    return DataLoader(dataprovider, batch_size=config["batch_size"], num_workers=config["num_workers"], shuffle=config["shuffle"])
+    return DataLoader(
+        dataprovider, 
+        batch_size=config["batch_size"], 
+        num_workers=config["num_workers"], 
+        shuffle=config["shuffle"]
+    ), dataprovider
