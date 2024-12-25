6a6e51a29a9c1dbe4fec1e4c22ace4485af9d8a4
akawincent <3511606256@qq.com>
2024-12-26 00:24:10 +0800

    feat: be able to replace key to its value

--- src/utils/load_config.py
+++ src/utils/load_config.py
@@ -1,15 +1,30 @@
 import yaml
 import argparse
 
-def load_yaml_config(file_path):
-    with open(file_path, 'r') as f:
-        config = yaml.safe_load(f)
-    return config
+def load_yaml_config(yaml_path):
+    with open(yaml_path, 'r') as f:
+        data = yaml.safe_load(f)
+    
+    logger_config = data['logger']
+    variables = {
+        'project': logger_config['project'],
+        'expname': logger_config['expname']
+    }
+    
+    for key, value in logger_config.items():
+        if isinstance(value, str):
+            temp_value = value  
+            for var_name, var_value in variables.items():
+                temp_value = temp_value.replace(f'${{project}}', variables['project'])
+                temp_value = temp_value.replace(f'${{expname}}', variables['expname'])
+            logger_config[key] = temp_value
+    
+    return data
 
 def parse_args():
     parser = argparse.ArgumentParser(description="configs for NeuEF")
 
-    parser.add_argument('--config', type=str, default='configs/test.yaml', 
+    parser.add_argument('--config', type=str, default='configs/synthetic.yaml', 
                         help="Path to the config file.")
     parser.add_argument('--gpu', type=int, default=0,
                         help='index of gpu to use')
