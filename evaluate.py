6ccc49d2f14ef2f463fe3656442916b4be237352
akawincent <3511606256@qq.com>
2024-12-26 19:04:26 +0800

    perf: evaluate batch optical flow

--- evaluate.py
+++ evaluate.py
@@ -3,38 +3,50 @@ import cv2 as cv2
 import numpy as np
 
 from tqdm import tqdm
+from pathlib import Path
 
+from src.utils import misc
 from src.utils import load_config
 from src.model.inr import EventFlowINR
 from src.utils.wandb import WandbLogger
 from src.utils.visualizer import Visualizer
+from src.model.geometric import KorniaPixel2Cam
 from src.utils.event_image_converter import EventImageConverter
 
-def compute_dense_flow(
-    model, t, 
-    grid_size, 
-    device="cuda",
-    normalize_coords=True
-):
-    H, W = grid_size
-    x, y = np.meshgrid(np.arange(W), np.arange(H))  
-    x = torch.tensor(x, dtype=torch.float32).view(-1)  
-    y = torch.tensor(y, dtype=torch.float32).view(-1)
+class DenseOpticalFlow:
+    def __init__(self, grid_size, model, normalize_coords=True, device="cuda"):
+        self.H, self.W = grid_size
+        x, y = np.meshgrid(np.arange(self.W), np.arange(self.H))
+        x = torch.tensor(x, dtype=torch.float32).view(-1)
+        y = torch.tensor(y, dtype=torch.float32).view(-1)
+        if normalize_coords:
+            x = x / (self.W - 1)
+            y = y / (self.H - 1)
+        self.x = x.to(device)
+        self.y = y.to(device)
 
-    if normalize_coords:
-        x = x / (W - 1)  
-        y = y / (H - 1) 
+        self.model = model
+        self.device = device
 
-    t = torch.full_like(x, t)  
-    txy = torch.stack((t, x, y), dim=1).to(device) 
-
-    with torch.no_grad():
-        flow = model(txy)  
-    u, v = flow[:, 0], flow[:, 1]
-    U = u.view(H, W)
-    V = v.view(H, W)
-    U, V = U * W, V * H
-    return u, v, x, y, U, V
+    def __call__(self, t):
+        num_points = self.x.shape[0]
+        num_timesteps = t.shape[1]
+        x_batch = self.x.repeat(num_timesteps)
+        y_batch = self.y.repeat(num_timesteps)
+        t_batch = t.view(-1).repeat_interleave(num_points).to(self.device)
+        txy = torch.stack((t_batch, x_batch, y_batch), dim=1)
+        
+        with torch.no_grad():
+            flow = self.model(txy)  # [num_points*num_timesteps, 2]
+        
+        u = flow[:, 0].view(num_timesteps, num_points)
+        v = flow[:, 1].view(num_timesteps, num_points)
+        U = u.view(num_timesteps, self.H, self.W)
+        V = v.view(num_timesteps, self.H, self.W)
+        
+        U, V = U * (self.W - 1), V * (self.H - 1)
+        
+        return u, v, U, V
 
 if __name__ == "__main__":
     args = load_config.parse_args()
@@ -65,30 +77,44 @@ if __name__ == "__main__":
     flow_field.eval() 
 
     # dense optical flow at any t
-    t = 1 
-    u, v, x, y, U, V = compute_dense_flow(flow_field, t, grid_size=image_size, device=device)
-
-    # visualize color optical flow
-    color_flow, wheel = viz.visualize_optical_flow(
-        flow_x=U.cpu().numpy(),
-        flow_y=V.cpu().numpy(),
-        visualize_color_wheel=True,
-        file_prefix="test_dense_optical_flow",
-        save_flow=False,
-        ord=1.0,
+    compute_dense_flow = DenseOpticalFlow(
+        image_size, 
+        flow_field, 
+        normalize_coords=True, 
+        device=device
     )
 
-    arrow_flow = viz.visualize_flow_arrows(
-        flow_x=U.cpu().numpy(),
-        flow_y=V.cpu().numpy(),  
-        file_prefix="optical_flow_arrow",
-        sampling_ratio=0.001, 
-        bg_color=(255, 255, 255)    
-    )
+    t = torch.linspace(0, 1, steps=50).view(1, -1)  # shape: [1, d]
+    u, v, U, V = compute_dense_flow(t)
 
-    # upload wandb
-    wandb_logger.write_img("color_optical_flow", color_flow)
-    wandb_logger.write_img("color_wheel", wheel)
-    wandb_logger.write_img("optical_flow_arrow", arrow_flow)
-    wandb_logger.update_buffer()
+
+    K_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_oneWall/K_matrix.txt")
+    pixel2cam = KorniaPixel2Cam(image_size[0], image_size[1], device)
+    K_tensor = misc.load_camera_intrinsic(K_path)
+    normalized_pixel_grid = pixel2cam(K_tensor)
+
+    for i in range(t.shape[1]):
+        # visualize color optical flow
+        color_flow, wheel = viz.visualize_optical_flow(
+            flow_x=U[i].cpu().numpy(),
+            flow_y=V[i].cpu().numpy(),
+            visualize_color_wheel=True,
+            file_prefix="test_dense_optical_flow",
+            save_flow=False,
+            ord=1.0,
+        )
+
+        arrow_flow = viz.visualize_flow_arrows(
+            flow_x=U[i].cpu().numpy(),
+            flow_y=V[i].cpu().numpy(),  
+            file_prefix="optical_flow_arrow",
+            sampling_ratio=0.001, 
+            bg_color=(255, 255, 255)    
+        )
+
+        # upload wandb
+        wandb_logger.write_img("color_optical_flow", color_flow)
+        wandb_logger.write_img("color_wheel", wheel)
+        wandb_logger.write_img("optical_flow_arrow", arrow_flow)
+        wandb_logger.update_buffer()
 
