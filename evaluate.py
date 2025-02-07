fix: verify the shape of hessian matrix of optimization is 6x6 and hessian matrix is dense matrix

evaluate.py
@@ -4,10 +4,14 @@ import numpy as np
 from tqdm import tqdm
 from pathlib import Path
 
-from src.utils import misc
-from src.utils import pose
+from src.utils import (
+    misc, 
+    pose, 
+    vector_math,
+    load_config
+)
+
 from src.model import geometric
-from src.utils import load_config
 from src.model.inr import EventFlowINR
 from src.utils.wandb import WandbLogger
 from src.utils.visualizer import Visualizer
@@ -39,9 +43,9 @@ if __name__ == "__main__":
     device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
     
     # load data
-    K_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_oneWall/K_matrix.txt")
-    CamPose_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_oneWall/camera_pose.txt")
-    TimeStamps_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_oneWall/timestamp.txt")
+    K_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_boxes/K_matrix.txt")
+    CamPose_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_boxes/camera_pose.txt")
+    TimeStamps_path = Path("/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_boxes/timestamp.txt")
     K_tensor = misc.load_camera_intrinsic(K_path)
     gt_camera_pose = misc.load_camera_pose(CamPose_path)
     timestamps = misc.load_time_stamps(TimeStamps_path)
@@ -59,7 +63,7 @@ if __name__ == "__main__":
         normalize_coords_mode="NORM_PLANE", 
         device=device
     )
-    sequence_length = 50
+    sequence_length = 19
     t = torch.linspace(0, 1, steps=sequence_length).view(1, -1).to(device)  # shape: [1, d]
     time_scale = 1 / timestamps[sequence_length - 1]
     # t_mid = ((t[0, 1:] + t[0, :-1]) / 2).unsqueeze(0) 
@@ -68,10 +72,10 @@ if __name__ == "__main__":
     pixel2cam = geometric.Pixel2Cam(image_size[0], image_size[1], device)
     normalized_pixel_grid = pixel2cam(K_tensor.to(device))
     normalized_pixel_grid = normalized_pixel_grid.squeeze(0)
-    # pose_optimizer = geometric.PoseOptimizer(image_size, device)
+    pose_optimizer = geometric.PoseOptimizer(image_size, device)
 
     # valid using gt depth
-    depth_folder = '/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_oneWall/depth'
+    depth_folder = '/run/determined/workdir/ssd_data/Event_Dataset/Blender/final_motion_boxes/depth'
     depth_paths = misc.get_sorted_txt_paths(depth_folder)
 
     for i in tqdm(range(t.shape[1])):
@@ -128,5 +132,37 @@ if __name__ == "__main__":
         wandb_logger.write_img("gt_optical_flow_arrow", gt_arrow_flow)
         wandb_logger.write_img("color_wheel", wheel)
         wandb_logger.update_buffer()
+        
+        
+        current_coords = normalized_pixel_grid.view(-1,3)
+        current_sparse_flow, indices = flow_calculator.sparsify_flow(
+            optical_flow,
+            sparse_ratio=0.01,
+            threshold=0.0001
+        )
+        current_sparse_coords = current_coords[indices, :]
+        
+        noise_level = 0.0
+        v_gt_noisy = v_gt + noise_level * torch.randn_like(v_gt, device=v_gt.device)
+        w_gt_noisy = w_gt + noise_level * torch.randn_like(w_gt, device=w_gt.device)
+        v_gt_noisy = v_gt_noisy / torch.norm(v_gt_noisy, p=2, dim=-1, keepdim=True)
+        init_velc = [v_gt_noisy, w_gt_noisy]
+        
+        v_est, w_est, v_his, w_his, err_his = pose_optimizer.optimize(current_sparse_coords, current_sparse_flow, only_rotation=False, init_velc=init_velc)
+        # _, w_est, _, w_his, err_his = pose_optimizer.optimize(current_sparse_coords, current_sparse_flow, only_rotation=True, init_velc=init_velc)   
+        v_est = v_est / torch.norm(v_est, p=2, dim=-1, keepdim=True)
+        print(f"======================================================= Pose Optimization =======================================================")
+        print(f"groundtruth_linear_velocity:{v_gt}, groundtruth_angular_velocity:{w_gt}")
+        print(f"initial_linear_velocity:{init_velc[0]}, intial_angular_velocity:{init_velc[1]}")
+        print(f"estimated_linear_velocity:{v_est}, estimated_angular_velocity:{w_est}")
+        print(f"linear_velocity_error:{vector_math.compute_vector_angle_in_degrees(v_est.to(device), v_gt)}, angular_velocity_error:{vector_math.compute_vector_angle_in_degrees(w_est.to(device), w_gt)}")
+        print(f"dec_last_error:{err_his[-1]}")
+        
+        # print(f"======================================================= Pose Optimization =======================================================")
+        # print(f"groundtruth_angular_velocity:{w_gt}")
+        # print(f"intial_angular_velocity:{init_velc[1]}")
+        # print(f"estimated_angular_velocity:{w_est}")
+        # print(f"angular_velocity_error:{vector_math.compute_vector_angle_in_degrees(w_est.to(device), w_gt)}")
+        # print(f"dec_last_error:{err_his[-1]}")
         
 