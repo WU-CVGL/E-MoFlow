4659f9f4849feddab7118e2c28962c7f0f313443
akawincent <3511606256@qq.com>
2025-03-27 17:04:00 +0800

    feat: upgrade warpper (torchdiffeq version)

--- src/model/warp.py
+++ src/model/warp.py
@@ -2,7 +2,9 @@ import torch
 import logging
 import torch.nn as nn
 from src.model.eventflow import EventFlowINR
-# from torchdiffeq import odeint_adjoint
+from torchdiffeq import odeint_adjoint
+from torchdiffeq import odeint
+import random
 
 EPS = 1e-5
 
@@ -40,7 +42,7 @@ class NeuralODEWarp:
         elif tref_setting == 'mid':
             return t_mid.unsqueeze(0)
         elif tref_setting == 'random':
-            return t_min + (t_max - t_min) * torch.rand(1).to(self.device)  # Uniform sampling between t_min and t_max
+            return t_min + (t_max - t_min) * random.uniform(0,1)  # Uniform sampling between t_min and t_max
         elif tref_setting == 'multi':
             return torch.stack([t_min, t_mid, t_max])  # Return all three values as a tensor
         else:
@@ -112,120 +114,120 @@ class NeuralODEWarp:
         # print(f"After warp: {warped_batch_txy[0,10000]}")
         return warped_batch_txy
 
-# class ODEFunc(nn.Module):
-#     def __init__(self, flow_field: EventFlowINR):
-#         super().__init__()
-#         self.flow_field = flow_field
+class ODEFunc(nn.Module):
+    def __init__(self, flow_field: EventFlowINR):
+        super().__init__()
+        self.flow_field = flow_field
         
-#     def forward(self, t_norm: torch.Tensor, augmented_state: torch.Tensor):
-#         x = augmented_state[..., 0]
-#         y = augmented_state[..., 1]
-#         t0 = augmented_state[..., 2]
-#         delta_t = augmented_state[..., 3]
+    def forward(self, t_norm: torch.Tensor, augmented_state: torch.Tensor):
+        x = augmented_state[..., 0]
+        y = augmented_state[..., 1]
+        t0 = augmented_state[..., 2]
+        delta_t = augmented_state[..., 3]
         
-#         t_real = t0 + t_norm * delta_t
+        t_real = t0 + t_norm * delta_t
         
-#         input_coords = torch.stack([
-#             t_real,
-#             x,
-#             y
-#         ], dim=-1)
+        input_coords = torch.stack([
+            t_real,
+            x,
+            y
+        ], dim=-1)
         
-#         flow = self.flow_field(input_coords)
+        flow = self.flow_field(input_coords)
         
-#         dxdt_norm = flow[..., 0] * delta_t
-#         dydt_norm = flow[..., 1] * delta_t
+        dxdt_norm = flow[..., 0] * delta_t
+        dydt_norm = flow[..., 1] * delta_t
         
-#         dt0dt = torch.zeros_like(t0)
-#         d_delta_t_dt = torch.zeros_like(delta_t)
+        dt0dt = torch.zeros_like(t0)
+        d_delta_t_dt = torch.zeros_like(delta_t)
         
-#         return torch.stack([
-#             dxdt_norm,
-#             dydt_norm,
-#             dt0dt,
-#             d_delta_t_dt
-#         ], dim=-1)
+        return torch.stack([
+            dxdt_norm,
+            dydt_norm,
+            dt0dt,
+            d_delta_t_dt
+        ], dim=-1)
 
-# class NeuralODEWarp(nn.Module):
-#     def __init__(self, flow_inr: EventFlowINR, device: torch.device, 
-#                  tref_setting: str, solver: str = 'rk4', rtol: float = 1e-7, 
-#                  atol: float = 1e-9) -> None:
-#         super().__init__()
-#         self.ode_func = ODEFunc(flow_inr)  
-#         self.flow_field = flow_inr
-#         self.device = device
-#         self.tref_setting = tref_setting
-#         self.solver = solver
-#         self.rtol = rtol
-#         self.atol = atol
+class NeuralODEWarpV2(nn.Module):
+    def __init__(self, flow_inr: EventFlowINR, device: torch.device, 
+                 tref_setting: str, solver: str = 'dopri5', rtol: float = 1e-7, 
+                 atol: float = 1e-9) -> None:
+        super().__init__()
+        self.ode_func = ODEFunc(flow_inr)  
+        self.flow_field = flow_inr
+        self.device = device
+        self.tref_setting = tref_setting
+        self.solver = solver
+        self.rtol = rtol
+        self.atol = atol
         
-#     def get_reference_time(self, batch_txy: torch.Tensor, tref_setting: str):
-#         """
-#         Get the value of t_ref based on the given setting.
+    def get_reference_time(self, batch_txy: torch.Tensor, tref_setting: str):
+        """
+        Get the value of t_ref based on the given setting.
 
-#         Args:
-#             batch_txy (torch.Tensor): A n*3 tensor [t,x,y].
-#             tref_setting (str): A string indicating the type of reference time to return.
-#                                 Options: 'min', 'max', 'mid', 'random', 'multi'.
+        Args:
+            batch_txy (torch.Tensor): A n*3 tensor [t,x,y].
+            tref_setting (str): A string indicating the type of reference time to return.
+                                Options: 'min', 'max', 'mid', 'random', 'multi'.
 
-#         Returns:
-#             torch.Tensor: A scalar or tensor based on tref_setting.
-#         """
-#         t_min = torch.min(batch_txy[:, 0]) + EPS
-#         t_max = torch.max(batch_txy[:, 0]) - EPS
-#         t_mid = (t_max + t_min) / 2
-
-#         if tref_setting == 'min':
-#             return torch.clamp(t_min, min=0.0, max=1.0).unsqueeze(0)
-#         elif tref_setting == 'max':
-#             return torch.clamp(t_max, min=0.0, max=1.0).unsqueeze(0)
-#         elif tref_setting == 'mid':
-#             return torch.clamp(t_mid, min=0.0, max=1.0).unsqueeze(0)
-#         elif tref_setting == 'random':
-#             return t_min + (t_max - t_min) * torch.rand(1).to(self.device)  # Uniform sampling between t_min and t_max
-#         elif tref_setting == 'multi':
-#             return torch.stack([t_min, t_mid, t_max])  # Return all three values as a tensor
-#         else:
-#             logger.error("Invalid tref_setting. Choose from ['min', 'max', 'mid', 'random', 'multi']")
-#             raise ValueError("Invalid tref_setting. Choose from ['min', 'max', 'mid', 'random', 'multi']")
+        Returns:
+            torch.Tensor: A scalar or tensor based on tref_setting.
+        """
+        t_min = torch.min(batch_txy[:, 0]) + EPS
+        t_max = torch.max(batch_txy[:, 0]) - EPS
+        t_mid = (t_max + t_min) / 2
+        
+        if tref_setting == 'min':
+            return t_min.unsqueeze(0)
+        elif tref_setting == 'max':
+            return t_max.unsqueeze(0)
+        elif tref_setting == 'mid':
+            return t_mid.unsqueeze(0)
+        elif tref_setting == 'random':
+            return t_min + (t_max - t_min) * random.uniform(0,1)  # Uniform sampling between t_min and t_max
+        elif tref_setting == 'multi':
+            return torch.stack([t_min, t_mid, t_max])  # Return all three values as a tensor
+        else:
+            logger.error("Invalid tref_setting. Choose from ['min', 'max', 'mid', 'random', 'multi']")
+            raise ValueError("Invalid tref_setting. Choose from ['min', 'max', 'mid', 'random', 'multi']")
     
-#     def _build_augmented_state(self, batch_txy: torch.Tensor, t_ref: torch.Tensor):
-#         t0 = batch_txy[:, 0]
-#         delta_t = t_ref - t0 
+    def _build_augmented_state(self, batch_txy: torch.Tensor, t_ref: torch.Tensor):
+        t0 = batch_txy[:, 0]
+        delta_t = t_ref - t0 
         
-#         spatial_coords = batch_txy[:, 1:]
+        spatial_coords = batch_txy[:, 1:]
         
-#         augmented_state = torch.cat([
-#             spatial_coords,
-#             t0.unsqueeze(-1),
-#             delta_t.unsqueeze(-1)
-#         ], dim=-1)
+        augmented_state = torch.cat([
+            spatial_coords,
+            t0.unsqueeze(-1),
+            delta_t.unsqueeze(-1)
+        ], dim=-1)
         
-#         return augmented_state
+        return augmented_state
     
-#     def warp_events(self, batch_txy: torch.Tensor, t_ref: torch.Tensor):
-#         # t_ref = self.get_reference_time(batch_txy, tref_setting)
+    def warp_events(self, batch_txy: torch.Tensor, t_ref: torch.Tensor):
+        # t_ref = self.get_reference_time(batch_txy, tref_setting)
         
-#         augmented_init = self._build_augmented_state(batch_txy, t_ref)
+        augmented_init = self._build_augmented_state(batch_txy, t_ref)
         
-#         t_eval = torch.tensor([0.0, 1.0], device=self.device)
+        t_eval = torch.tensor([0.0, 1.0], device=self.device)
         
-#         solution = odeint_adjoint(
-#             self.ode_func,
-#             augmented_init,
-#             t_eval,
-#             method=self.solver,
-#             options={
-#                 "step_size": 10
-#             }
-#             # rtol=self.rtol,
-#             # atol=self.atol
-#         )
+        solution = odeint(
+            self.ode_func,
+            augmented_init,
+            t_eval,
+            method=self.solver,
+            options={
+                "step_size": 1
+            },
+            rtol=self.rtol,
+            atol=self.atol
+        )
         
-#         final_state = solution[-1]
-#         warped_spatial = final_state[:, :2]
+        final_state = solution[-1]
+        warped_spatial = final_state[:, :2]
         
-#         return torch.cat([
-#             t_ref.expand(batch_txy.size(0)).unsqueeze(-1),  # 保持时间维度
-#             warped_spatial
-#         ], dim=-1)
+        return torch.cat([
+            t_ref.expand(batch_txy.size(0)).unsqueeze(-1),  # 保持时间维度
+            warped_spatial
+        ], dim=-1)
