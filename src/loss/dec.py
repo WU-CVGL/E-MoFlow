510147727cf0f19e9f9422aadfc7efd477cd22e1
akawincent <liwenpu0520@gmail.com>
2025-07-30 18:13:04 +0800

    pref: add huber loss to robustify the outliers

--- src/loss/dec.py
+++ src/loss/dec.py
@@ -4,9 +4,24 @@ import torch.nn as nn
 from src.utils.vector_math import vec2skewmat
 
 class DifferentialEpipolarLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, delta=1.0, use_huber=True):
         super(DifferentialEpipolarLoss, self).__init__()
+        self.delta = delta  
+        self.use_huber = use_huber  
 
+    def huber_loss(self, error):
+        """
+        Compute Huber loss
+        Args:
+            error: Error tensor
+        Returns:
+            Huber loss
+        """
+        abs_error = torch.abs(error)
+        quadratic = torch.minimum(abs_error, torch.tensor(self.delta))
+        linear = abs_error - quadratic
+        return 0.5 * quadratic**2 + self.delta * linear
+    
     def forward(self, coord, flow, lin_vel, ang_vel):
         """
         Args:
@@ -33,7 +48,12 @@ class DifferentialEpipolarLoss(nn.Module):
         term2 = torch.einsum("in,ij,jn->n", x_batch_tensor, s, x_batch_tensor)
 
         error = term1 - term2
-        dec_loss = torch.square(error)
+        
+        if self.use_huber:
+            dec_loss = self.huber_loss(error)
+        else:
+            dec_loss = torch.square(error)
+            
         average_dec_loss = torch.mean(dec_loss)
 
         return dec_loss, average_dec_loss
